e05e04631817119a72531d4d09b8e9132c345901
gkum4 <gustavokumasawa@gmail.com>
2025-09-24 13:21:18 -0300

    ViewCode configuration

--- DogsBreeds/ViewController.swift
+++ DogsBreeds/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let label = UILabel()
+        label.text = "ViewController"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
-
-
 }
 
