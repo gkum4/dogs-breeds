4d5d63a5948df960e9e3b4eecee94782cfff4398
gkum4 <gustavokumasawa@gmail.com>
2025-09-25 13:56:56 -0300

    feat: ApiProtocol extension for default JSONDecoder

--- Networking/Sources/Networking/Sources/Protocols/ApiProtocol.swift
+++ Networking/Sources/Networking/Sources/Protocols/ApiProtocol.swift
@@ -4,3 +4,9 @@ public protocol ApiProtocol {
     func execute<T: Decodable>(endpoint: ApiEndpointProtocol,
                                decoder: JSONDecoder) async -> Result<T, ApiError>
 }
+
+public extension ApiProtocol {
+    func execute<T: Decodable>(endpoint: ApiEndpointProtocol) async -> Result<T, ApiError> {
+        await execute(endpoint: endpoint, decoder: JSONDecoder())
+    }
+}
